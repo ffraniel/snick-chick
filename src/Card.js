82526cf9af45fda778a61a0e2ccf5f4c8f7ba45d
ffraniel <francis.whitehead@googlemail.com>
2019-04-04 16:16:05 +0100

    change cards by clicking selection

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({chicken, highlighted}) => {
+const Card = ({ chicken, highlighted, changeCurrent }) => {
   const { breed, img, details } = chicken;
   const highlightedClass = highlighted ? 'Card-Highlighted' : null;
 
   return (
-    <div className={`Chicken-Card ${highlightedClass}`}>
+    <div className={`Chicken-Card ${highlightedClass}`} onClick={()=>{changeCurrent(chicken.index)}} >
       <img className="Chicken-Image" alt={`Chicken of the ${breed} variety.`} src={img} />
       <h1 className="Chicken-Name">{breed}</h1>
       <p className="Chicken-Details">{details}</p>
