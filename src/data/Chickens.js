d0f5d44c41e54e4dc1a21d2d858cb4c4fd63464e
ffraniel <francis.whitehead@googlemail.com>
2019-04-05 01:57:30 +0100

    updated chicke data

--- src/data/Chickens.js
+++ src/data/Chickens.js
@@ -19,6 +19,18 @@ const Chickens = [
   },
   {
     index: 3,
+    breed: "Cream Legbar",
+    img: "assets/LEGBAR.jpeg",
+    details: "The Legbar was created in the early twentieth century by Reginald Crundall Punnett and Michael Pease at the Genetical Institute of Cambridge University. It was created by cross-breeding Barred Plymouth Rock chicken, Leghorns, Cambars, and in the case of Cream Legbars, Araucanas."
+  },
+  {
+    index: 4,
+    breed: "Little Brown Hen",
+    img: "assets/LBH.jpg",
+    details: "Little Brown Hens are the regular, run of the mill, par for the course chicken about town. They can spin an egg or two, like a mellow life and don't kick up too much of a fuss."
+  },
+  {
+    index: 5,
     breed: "Muscovy",
     img: "assets/MUSC.jpg",
     details: "It is allocated to a subgroup of perching ducks known as ‘the greater wood ducks’, which it shares with the white-winged wood duck and Hartlaub’s duck. These are heavy-bodied birds with relatively short legs that give them a horizontal carriage. They have broad wings with bony knobs at the carpal joints."
